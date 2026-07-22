aeb2665ae5351be3666367de5d22720aec779fa6
rivermh <kmh000904@gmail.com>
2026-07-22 13:56:25 +0900

    first commit

---
+++ soratrip/src/main/java/com/rivermh/soratrip/SoratripApplication.java
@@ -0,0 +1,13 @@
+package com.rivermh.soratrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoratripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoratripApplication.class, args);
+	}
+
+}
---
+++ soratrip/src/test/java/com/rivermh/soratrip/SoratripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivermh.soratrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoratripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
